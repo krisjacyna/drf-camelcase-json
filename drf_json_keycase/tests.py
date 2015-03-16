7b38cb5425114fd2aec3ea7df96719ab769d7cfe
Kris Jacyna <kjacyna1@gmail.com>
2015-03-16 20:31:21 +0000

    Added to_snakecase and associated tests

--- drf_json_keycase/tests.py
+++ drf_json_keycase/tests.py
@@ -3,7 +3,7 @@ import transformations
 
 class TestToCase(unittest.TestCase):
 
-    def test_str(self):
+    def test_str_to_case(self):
         cases = (
             'camels_have_humps',
             'camels_haveHumps',
@@ -20,12 +20,30 @@ class TestToCase(unittest.TestCase):
             self.assertEqual('CamelsHaveHumps', transformations.str_to_pascalcase(case))
 
 
+    def test_str_to_snake(self):
+        cases = (
+            'camelsHaveHumps',
+            'CamelsHaveHumps',
+            'camelsHAVEHumps',
+            'camelsHaveHUMPS',
+            'CAMELSHaveHumps',
+        )
+        for case in cases:
+            self.assertEqual('camels_have_humps', transformations.str_to_snakecase(case))
+
+
     def test_dict_keys(self):
         d = {'key_one': 1, 'key_two': 2}
         cd = {'keyOne': 1, 'keyTwo': 2}
         pd = {'KeyOne': 1, 'KeyTwo': 2}
+
+        # to camel/pascal
         self.assertEqual(cd, transformations.keys_to_camelcase(d))
         self.assertEqual(pd, transformations.keys_to_pascalcase(d))
+
+        # to snake
+        self.assertEqual(d, transformations.keys_to_snakecase(cd))
+        self.assertEqual(d, transformations.keys_to_snakecase(pd))
 
 
     def test_nested_dict_keys(self):
@@ -33,13 +51,24 @@ class TestToCase(unittest.TestCase):
         cd = {'keyOne': 1, 'keyTwo': 2, 'keyThree': {'nestData': 'n'}}
         pd = {'KeyOne': 1, 'KeyTwo': 2, 'KeyThree': {'NestData': 'n'}}
 
+        # to camel/pascal
         self.assertEqual(cd, transformations.keys_to_camelcase(d))
         self.assertEqual(pd, transformations.keys_to_pascalcase(d))
+
+        # to snake
+        self.assertEqual(d, transformations.keys_to_snakecase(cd))
+        self.assertEqual(d, transformations.keys_to_snakecase(pd))
 
 
     def test_nested_list_dict_keys(self):
         d = {'key_one': 1, 'key_two': [{'nest_data_one': 1}, {'nest_data_two': 2}]}
         cd = {'keyOne': 1, 'keyTwo': [{'nestDataOne': 1}, {'nestDataTwo': 2}]}
         pd = {'KeyOne': 1, 'KeyTwo': [{'NestDataOne': 1}, {'NestDataTwo': 2}]}
+
+        # to camel/pascal
         self.assertEqual(cd, transformations.keys_to_camelcase(d))
         self.assertEqual(pd, transformations.keys_to_pascalcase(d))
+
+        # to snake
+        self.assertEqual(d, transformations.keys_to_snakecase(cd))
+        self.assertEqual(d, transformations.keys_to_snakecase(pd))
--- drf_json_keycase/transformations.py
+++ drf_json_keycase/transformations.py
@@ -1,5 +1,8 @@
 import re
 
+FIRST_CAP = re.compile('(.)([A-Z][a-z]+)')
+ALL_CAP = re.compile('([a-z0-9])([A-Z])')
+
 def _underscore_repl(match):
     return match.group()[1].upper()
 
@@ -28,6 +31,16 @@ def str_to_pascalcase(str_value):
     # upcase first letter in case it was lowercase
     pascal = pascal[0].upper() + pascal[1:]
     return pascal
+
+
+def str_to_snakecase(str_value):
+    """
+    Transforms a camel/pascal case string value to snake case, where underscores
+    are inserted between each lowercase/uppercase pair. All characters returned
+    will be lowercase.
+    """
+    s = FIRST_CAP.sub(r'\1_\2', str_value)
+    return ALL_CAP.sub(r'\1_\2', s).lower()
 
 
 def _transform_keys(data, key_transformer):
@@ -60,3 +73,11 @@ def keys_to_pascalcase(data):
     where the keys are replaced with pascal case equivalents.
     """
     return _transform_keys(data, str_to_pascalcase)
+
+
+def keys_to_snakecase(data):
+    """
+    Returns a recursively transformed copy of a string keyed dict
+    where the keys are replaced with snake case equivalents.
+    """
+    return _transform_keys(data, str_to_snakecase)
